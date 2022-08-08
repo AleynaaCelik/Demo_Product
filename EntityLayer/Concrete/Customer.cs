27b3c75a9a84f7ee0027e2de58dfe2561dd9c01e
Aleyna06 <celikaleyna71@gmail.com>
2022-08-08 15:14:28 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+   public  class Customer
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String City { get; set; }
+    }
+}
