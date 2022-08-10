5947dd6cfdb78ef36890c6c661d1b7594ee61773
Aleyna06 <celikaleyna71@gmail.com>
2022-08-10 11:59:47 +0300

    Generic Repo

---
+++ DataAcessLayer/EntityFramework/EfCustomerDal.cs
@@ -0,0 +1,15 @@
+﻿using DataAcessLayer.Abstract;
+using DataAcessLayer.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAcessLayer.EntityFramework
+{
+   public class EfCustomerDal : GenericRepository<Customer>, ICustomerDal
+    {
+    }
+}
