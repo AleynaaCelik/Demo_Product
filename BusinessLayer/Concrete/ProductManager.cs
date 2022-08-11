c9b64f02902ae9dd221a149acdb0de9d5dd2cb5c
Aleyna06 <celikaleyna71@gmail.com>
2022-08-11 10:34:05 +0300

    Fluent validition

---
+++ BusinessLayer/Concrete/ProductManager.cs
@@ -0,0 +1,48 @@
+﻿using BusinessLayer.Abstract;
+using DataAcessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class ProductManager :  IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            //Ypıcı method ataması yaptım
+            _productDal = productDal;
+        }
+
+        public void TDelete(Product t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product TGetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> TGetlist()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TInsert(Product t)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public void TUpdate(Product t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
