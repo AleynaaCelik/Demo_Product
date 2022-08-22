5fa22d9d2f5b8e846b0ebc5c806fc5f64f539128
Aleyna06 <celikaleyna71@gmail.com>
2022-08-22 16:37:40 +0300

    Register İşlemi

--- Demo_Product/Startup.cs
+++ Demo_Product/Startup.cs
@@ -1,3 +1,5 @@
+using DataAcessLayer.Concrete;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -23,6 +25,8 @@ namespace Demo_Product
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<Context>();
+            services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<Context>();
             services.AddControllersWithViews();
         }
 
