6ae2f75818719e60b0f6f81805086b9953a97187
Aleyna06 <celikaleyna71@gmail.com>
2022-08-22 11:48:19 +0300

    Register View Model

--- Demo_Product/Controllers/RegisterController.cs
+++ Demo_Product/Controllers/RegisterController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Demo_Product.Models;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +11,45 @@ namespace Demo_Product.Controllers
 {
     public class RegisterController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(UserRegisterViewModel model)
+        {
+            AppUser _appUser = new AppUser()
+            {
+                Name = model.Name,
+                Surname = model.SurName,
+                UserName = model.UserName,
+                Email=model.Mail
+            };
+
+            if (model.Password == model.ConfirmPassword)
+            {
+                var result = await _userManager.CreateAsync(_appUser, model.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index","Login");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+            return View(model);
         }
     }
 }
