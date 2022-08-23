6fb6c4f5bfc955125e30d001a010af52b11fcb24
Aleyna06 <celikaleyna71@gmail.com>
2022-08-23 14:11:36 +0300

    Login Backend Kısmı

---
+++ Demo_Product/Controllers/LoginController.cs
@@ -0,0 +1,42 @@
+﻿using Demo_Product.Models;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo_Product.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public LoginController(SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task <IActionResult> Index(UserLoginViewModel p)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(p.UserName,p.Password,false,true);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index","Category");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Hatalı Kullanıcı adı veya şifre");                }
+            }
+            return View();
+        }
+    }
+}
---
+++ Demo_Product/Models/UserLoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo_Product.Models
+{
+    public class UserLoginViewModel
+    {
+        [Required(ErrorMessage ="Lütfen Kullanıcı Adınızı giriniz")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage ="Lütfen Şifrenizi giriniz")]
+        public string Password { get; set; }
+    }
+}
